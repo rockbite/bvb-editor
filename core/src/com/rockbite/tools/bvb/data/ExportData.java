0947601506bcc8a49ab6c59f3941cc1d6718fda9
vuvkar <vahe.karamyan@rockbitegames.com>
2019-09-22 21:36:56 +0400

    fixed kryo shit

--- core/src/com/rockbite/tools/bvb/data/ExportData.java
+++ core/src/com/rockbite/tools/bvb/data/ExportData.java
@@ -10,7 +10,12 @@ import java.util.Map;
 
 public class ExportData {
 
-    public HashMap<String, HashMap<String, Array<VFXExportData>>> boundVFXList = new HashMap<String, HashMap<String, Array<VFXExportData>>>();
+
+    //this is for supporting old version
+    @Deprecated
+    private HashMap<String, Array<VFXExportData>> boundVFXList = new HashMap<String, Array<VFXExportData>>();
+
+    public HashMap<String, HashMap<String, Array<VFXExportData>>> newBoundVFXList = new HashMap<String, HashMap<String, Array<VFXExportData>>>();
 
     public Array<SFXExportData> sfxExportData = new Array<SFXExportData>();
 
@@ -22,7 +27,7 @@ public class ExportData {
             xml.element("bindings");
             xml.element("vfx");
 
-            for (Map.Entry<String,HashMap<String, Array<VFXExportData>>> entry : boundVFXList.entrySet()) {
+            for (Map.Entry<String,HashMap<String, Array<VFXExportData>>> entry : newBoundVFXList.entrySet()) {
                 String skinName = entry.getKey();
                 HashMap<String, Array<VFXExportData>> animations = entry.getValue();
                 for (Map.Entry<String, Array<VFXExportData>> animationData : animations.entrySet()) {
